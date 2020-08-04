6e4b4f048cb39dc7c176763654b4c0903f84ade7
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2020-08-04 15:58:02 +0200

    minor changes

--- src/main/java/utils/EMF_Creator.java
+++ src/main/java/utils/EMF_Creator.java
@@ -1,10 +1,6 @@
 package utils;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -13,7 +9,7 @@ public class EMF_Creator {
     /**
      * Call this method before all integration tests that uses the Grizzly
      * Server and the Test Database (in @BeforeAll ) Remember to call
-     * enRestTestWithDB() (in @AfterAll)
+     * enRestTestWithDB() in @AfterAll
      */
     public static void startREST_TestWithDB() {
         System.setProperty("IS_INTEGRATION_TEST_WITH_DB", "testing");
@@ -36,28 +32,34 @@ public class EMF_Creator {
 
     private static EntityManagerFactory createEntityManagerFactory(boolean isTest) {
 
-        //Check if we are in production and use credentials passed in on the server via CATALINA_OPTS        
-        if (System.getenv("CATALINA_OPTS") != null) {
-            String user = System.getProperty("USER");
-            String pw = System.getProperty("PW");
-            String connection_str = System.getProperty("CONNECTION_STR");
-            if(user == null || pw == null || connection_str==null){
-                throw new RuntimeException("one or all of the values used to setup the database is missing");
-            }
+        
+        /* Strategy for deployment
+           Same as in the old version of the start code. That is, environtment variables must be set in
+           setenv.sh on the server
+        */
+        boolean isDeployed = (System.getenv("DEPLOYED") != null);
+        if(isDeployed){
+            System.out.println("USING ENVIRONMENT VARIABLES");
+            String user = System.getenv("USER");
+            String pw = System.getenv("PW");
+            String connection_str = System.getenv("CONNECTION_STR");
             Properties props = new Properties();
             props.setProperty("javax.persistence.jdbc.user", user);
             props.setProperty("javax.persistence.jdbc.password", pw);
             props.setProperty("javax.persistence.jdbc.url", connection_str);
             return Persistence.createEntityManagerFactory("pu", props);
-
         }
-
-        String puName = isTest || System.getProperty("IS_INTEGRATION_TEST_WITH_DB") != null ? "puTest" : "pu"; //Only legal name
+        
+       
+        /* Strategy for dev and test
+           Uses the two persistence units declared in persistence.xml
+        */
+               
+        String puName = isTest || System.getProperty("IS_INTEGRATION_TEST_WITH_DB") != null ? "puTest" : "pu"; //Only legal names
         if (puName.equals("puTest")) {
-            System.out.println("Using the test database via puTest ");
+            System.out.println("Using the TEST database via puTest ");
         } else {
-
-            System.out.println("Using the dev database via pu ");
+            System.out.println("Using the DEV database via pu ");
         }
         return Persistence.createEntityManagerFactory(puName, null);
     }
