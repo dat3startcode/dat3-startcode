7bd63070a7285540e8901633348f84d9151565b0
lars-m <lam@cphbusiness.dk>
2020-10-16 17:22:16 +0200

    security features added

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -20,8 +20,15 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.DemoResource.class);
         resources.add(rest.RenameMeResource.class);
+        resources.add(security.JWTAuthenticationFilter.class);
+        resources.add(security.LoginEndpoint.class);
+        resources.add(security.RolesAllowedFilter.class);
+        resources.add(security.errorhandling.AuthenticationExceptionMapper.class);
+        resources.add(security.errorhandling.NotAuthorizedExceptionMapper.class);
     }
     
 }
