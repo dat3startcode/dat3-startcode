cf32f22a280e66fdd631750e9fe4e3eb06f03f28
Thomas Hartmann <thomas@webtrade.dk>
2021-09-14 10:24:36 +0200

    minor update to RenameMeDTO constructor, when running populator it failed initially

--- src/main/java/facades/Populator.java
+++ src/main/java/facades/Populator.java
@@ -21,7 +21,6 @@ public class Populator {
         fe.create(new RenameMeDTO(new RenameMe("First 1", "Last 1")));
         fe.create(new RenameMeDTO(new RenameMe("First 2", "Last 2")));
         fe.create(new RenameMeDTO(new RenameMe("First 3", "Last 3")));
-        
     }
     
     public static void main(String[] args) {
