ea226124c9e4dde0b6efb503a20a719656a85d64
Thomas Hartmann <thomas@webtrade.dk>
2021-09-30 13:19:42 +0200

    removed the wadl file from applicationConfig

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -28,7 +28,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(errorhandling.GenericExceptionMapper.class);
-        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+//        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.RenameMeResource.class);
     }
     
