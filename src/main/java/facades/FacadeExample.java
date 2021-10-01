8070cfe07c6a309027106804fa815365c1d8c1f6
Thomas Hartmann <thomas@webtrade.dk>
2021-10-01 11:37:12 +0200

    minor error

--- src/main/java/facades/FacadeExample.java
+++ src/main/java/facades/FacadeExample.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
 
-import errorhandling.RenameMeNotFoundException;
+//import errorhandling.RenameMeNotFoundException;
 import utils.EMF_Creator;
 
 /**
