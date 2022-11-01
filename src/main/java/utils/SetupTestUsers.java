5331d32c7f9cd89f990469a63c2ea3b2e8cd95e5
Thomas Hartmann <thomas@webtrade.dk>
2022-11-01 10:24:20 +0100

    added http

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -20,9 +20,9 @@ public class SetupTestUsers {
     // Also, either delete this file, when users are created or rename and add to .gitignore
     // Whatever you do DO NOT COMMIT and PUSH with the real passwords
 
-    User user = new User("user", "test");
-    User admin = new User("admin", "test");
-    User both = new User("user_admin", "test");
+    User user = new User("user", "test123");
+    User admin = new User("admin", "test123");
+    User both = new User("user_admin", "test123");
 
     if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
       throw new UnsupportedOperationException("You have not changed the passwords");
