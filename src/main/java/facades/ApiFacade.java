ec1be04869da708aa2408297a175a8974d5ac56d
Thomas Hartmann <thomas@webtrade.dk>
2023-04-20 11:28:12 +0200

    added atypical json response example wit dad jokes

--- src/main/java/facades/ApiFacade.java
+++ src/main/java/facades/ApiFacade.java
@@ -2,6 +2,7 @@ package facades;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,7 +14,7 @@ import java.util.Arrays;
 public class ApiFacade {
     // Purpose of this class is to demo how to read data from an external API
     // and return it as a DTO
-    public static void main(String[] args) throws IOException {
+    public static void zipRequester() throws IOException {
         Gson GSON = new GsonBuilder().create();
         String urlString = "https://api.dataforsyningen.dk/postnumre";
         URL yahoo = new URL(urlString);
@@ -28,11 +29,50 @@ public class ApiFacade {
         in.close();
 
 //      System.out.println(sb); // Uncomment to see the raw JSON data. This will help you to create the DTO properly!!
-        MyDTO[] myDtos = GSON.fromJson(sb.toString(), MyDTO[].class);
+        ZipDTO[] myDtos = GSON.fromJson(sb.toString(), ZipDTO[].class);
         Arrays.stream(myDtos).forEach(dto->System.out.println(dto.nr + " " + dto.navn));
     }
-    class MyDTO {
+    class ZipDTO {
         public String nr;
         public String navn;
+    }
+
+    class  JokeDTO {
+        JsonObject[] body;
+    }
+
+    public static String jokesAPI(String apiKey) throws IOException {
+        // Purpose of this example is to show an atypical json response (dealing with an array and JsonObject)
+        // In order to run REGISTER TO GET API KEY HERE: https://rapidapi.com/KegenGuyll/api/dad-jokes/
+        Gson GSON = new GsonBuilder().create();
+        String urlString = "https://dad-jokes.p.rapidapi.com/random/joke";
+        URL url = new URL(urlString);
+        URLConnection urlConnection = url.openConnection();
+
+        urlConnection.setRequestProperty("X-RapidAPI-Key", apiKey);
+        urlConnection.setRequestProperty("X-RapidAPI-Host", "dad-jokes.p.rapidapi.com");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        String inputLine;
+        StringBuilder sb = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null)
+            sb.append(inputLine);
+        System.out.println(sb);
+        in.close();
+
+        JokeDTO myDTO = GSON.fromJson(sb.toString(), JokeDTO.class);
+        System.out.println(myDTO.body[0].get("setup"));
+
+        return myDTO.body[0].get("setup").toString() + " " + myDTO.body[0].get("punchline").toString();
+    }
+
+    public static void main(String[] args) {
+        try {
+            zipRequester();
+            jokesAPI("registered api key here");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
